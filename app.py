90dd7e3068aeeb8024220ec2f97159de014e8dde
haashimi <mohameddiini@outlook.com>
2023-06-07 11:55:42 +0300

    app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-
+import os
 import json
+from io import BytesIO
 from api  import RestApiSdk
+from dotenv import load_dotenv
+
+class Client:
+    def __init__(self):
+        self.api = RestApiSdk('base_url') 
+        load_dotenv()
 
 
 
-def __init__(self):
-    self.api = RestApiSdk('base_url') 
+    def sendRequest(self):
+        
+        payload = {
 
+        }
+        headers = {
+        'Content-Type': 'application/json'
+        }
 
+        response = self.api.post('/endpoint', data=payload, headers=headers)
 
-def sendRequest(self):
-    
-    payload = {
-
-    }
-    headers = {
-    'Content-Type': 'application/json'
-    }
-
-    response = self.api.post('/endpoint', data=payload, headers=headers)
-
-    # print(response)
-    
-    return response
-
+        # print(response)
+        
+        return response
