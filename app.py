021e400b160a2c69d15dc2df7069bf3b1ba81581
haashimi <mohameddiini@outlook.com>
2023-06-08 13:31:29 +0300

    app.py

--- app.py
+++ app.py
@@ -6,21 +6,46 @@ from dotenv import dotenv_values
 
 class Client:
     def __init__(self):
-        self.api = RestApiSdk('base_url') 
+        
         self.config = dotenv_values(".env")
-
-
-    def sendRequest(self):
-        
-        payload = {
-
-        }
-        headers = {
-        'Content-Type': 'application/json'
+        self.api = RestApiSdk(self.config['url']) 
+        self.headers = {
+        'api_access_token': self.config['apikey']
         }
 
-        response = self.api.post('/endpoint', data=payload, headers=headers)
 
-     
-        
-        return response
+    def createMediaMessage(self, account_id, conversation_id, ):
+        try:
+            payload = {'message_type': 'incoming'}
+
+            response = self.api.post(f'{account_id}/conversations/{conversation_id}/messages', data=payload, headers=self.headers)
+            return response
+        except Exception  as e:
+            
+            print(f"An error occurred CreateContact: {str(e)}")
+
+            return str(e)
+            
+
+    def createMessage(self, account_id, conversation_id, message):
+        try:
+            payload = { 'content': message, 'message_type': 'incoming' }
+
+            response = self.api.post(f'{account_id}/conversations/{conversation_id}/messages', data = payload, headers=self.headers)
+            print(response.text)
+            return response
+        except Exception  as e:
+            # Handle any exceptions that occur during the execution of the code
+            print(f"An error occurred CreateContact: {str(e)}")
+
+            return str(e)
+
+
+
+
+ 
+
+
+
+
+
